f1581db802140308996a90e02974dbe287289f23
joonsup-0529 <80247562+joonsup-0529@users.noreply.github.com>
2021-03-18 17:04:05 +0900

    Create pointer_202131424.cpp

---
+++ pointer_202131424.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int x;
+  int *p;
+  
+  p = &x;
+  cin >>  x;
+  cin.ignore();
+  cout << *p << "\n";
+  cin.get();
+  
+  return 0;
+  
+  
+}
